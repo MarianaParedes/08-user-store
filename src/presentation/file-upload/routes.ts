ca16c5f89b99f3fa4fb934f6ba1bac983f623132
Mariana <mariana.andrea.paredes@gmail.com>
2025-01-03 18:47:37 -0300

    cap 22 video 321

---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { FileUploadController } from './controller';
+import { FileUploadService } from '../services/file-upload.service';
+import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
+
+
+export class FileUploadRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+    const controller = new FileUploadController(
+      new FileUploadService()
+    );
+
+
+    router.use( FileUploadMiddleware.containFiles );
+    // Definir las rutas
+    // api/upload/single/<>
+    router.post('/single/:type', controller.uploadFile );
+    router.get('/multiple/:type', controller.uploadMultipleFiles );
+
+    return router;
+
+  }
+
+}
+
